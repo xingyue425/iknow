fd3565a322f412a103a3e31fb2c523f49d54d2f9
xingyue425 <2006429019zk@163.com>
2020-07-21 17:02:34 +0800

    添加多事务。

---
+++ know-atomikos/src/main/java/com/zk/knowatomikos/KnowAtomikosApplication.java
@@ -0,0 +1,13 @@
+package com.zk.knowatomikos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowAtomikosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowAtomikosApplication.class, args);
+    }
+
+}
---
+++ know-atomikos/src/test/java/com/zk/knowatomikos/KnowAtomikosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zk.knowatomikos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowAtomikosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
